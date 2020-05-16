50f5d29c7962e952ad3c67d3ab0e66babb51dec5
spaceback1999 <spaceback1999@gmail.com>
2020-05-16 12:35:25 +0700

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const mongoose = require('mongoose');
 const studentRouter = require('./routes/student');
 const bodyParser = require('body-parser');
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUi=require('swagger-ui-express');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -21,5 +23,38 @@ mongoose.connect(DB_URL)
 .catch (e => {
     console.log(e);
 })
+
+const swaggerOptions={
+    swaggerDefinition: {
+        info: {
+            title: 'My name',
+            description: "Hoàng Minh Phát",
+            version: "1.0.0",
+            contact: {
+                name: "Hoàng Minh Phát",
+                email: "17520876@gm.edu.vn",
+            },
+            servers: ["localhost:8888"]
+        }
+    },
+    apis: ["app.js"]
+};
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/apidocs',swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+
+// Bătddaufw viết api doc nay
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: ...
+ *      description: ...
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          '200': 
+ *              description: ....
+ */
 
 app.use(express.static(path.join(__dirname, 'public')));
